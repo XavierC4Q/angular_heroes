ae94824eef6008596c5a5f437235466bcf078b2c
Xavier <munroexavier@gmail.com>
2019-02-21 13:28:28 -0500

    first angular

---
+++ src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Hero } from '../classes/hero';
+import { HEROES } from '../mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  getHeroes(): Observable<Hero []> {
+    this.messageService.add('Hero Service: Got heroes');
+    return of(HEROES);
+  }
+constructor(private messageService: MessageService) { }
+
+}
---
+++ src/mock-heroes.ts
@@ -0,0 +1,7 @@
+import { Hero } from './classes/hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Xavier', power: 'Pyrokinesis'},
+    { id: 2, name: 'Monique', power: 'Super strength'},
+    { id: 3, name: 'Matthew', power: 'Friction manipulation'}
+];
