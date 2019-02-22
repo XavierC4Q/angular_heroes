c78065382e194be032725a1a5df89744b795f80f
Xavier <munroexavier@gmail.com>
2019-02-21 23:43:23 -0500

    finished angular tutorial. going to style this up and play with it.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryStorageService } from './in-memory-storage.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { HttpClientModule } from '@angular/common/http';
       HeroesComponent,
       HeroDetailComponent,
       MessagesComponent,
-      DashboardComponent
+      DashboardComponent,
+      HeroSearchComponent
    ],
    imports: [
       BrowserModule,
